f879b62ca32db3569e8926b6b772cbf7a9b58d16
jacobhsu <jacob.hsu.tw@gmail.com>
2022-04-26 10:50:34 +0800

    feat: NotFound

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,16 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "./pages/home";
+import NotFound from "./pages/notFound";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home />,
+    children: [],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
     children: [],
   },
 ];
