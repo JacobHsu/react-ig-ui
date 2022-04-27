9febd0121ec71c5e110154a3e6de7aa2512d3a8d
jacobhsu <jacob.hsu.tw@gmail.com>
2022-04-27 10:02:07 +0800

    feat: following

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,22 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "./pages/home";
+import Following from "./pages/following";
 import NotFound from "./pages/notFound";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home />,
+    children: [],
+  },
+  {
+    path: "/react-ig-ui",
+    element: <Home />,
+    children: [],
+  },
+  {
+    path: "/following",
+    element: <Following />,
     children: [],
   },
   {
