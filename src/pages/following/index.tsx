7ce3a2893497cfca99a69c49d4dc22548c1c591e
jacobhsu <jacob.hsu.tw@gmail.com>
2022-05-06 10:30:33 +0800

    feat: pages Following

--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -1,5 +1,36 @@
+import IGContainer from "../../components/IGContainer";
+import IGHeader from "../../components/IGHeader";
+import IGUser from "../../components/IGUser";
+import { friends } from "../../Friends";
+
 const Following: React.FC = () => {
-  return <p>Following</p>;
+
+  return (
+    <>
+      <IGHeader />
+      <IGContainer>
+        <p className="box-border px-4 my-8 text-2xl font-bold">Following</p>
+        <div className="box-border mx-2 mt-8 shadow-md">
+          {friends.map((item) => {
+            const { id, account, avatar, isFollowing, location } = item;
+            return (
+              <div className="-mt-3">
+                <IGUser
+                  key={id}
+                  id={id}
+                  account={account}
+                  avatar={avatar}
+                  location={location}
+                  isFollowing={isFollowing}
+                  showFollow
+                />
+              </div>
+            );
+          })}
+        </div>
+      </IGContainer>
+    </>
+  );
 };
 
 export default Following;
