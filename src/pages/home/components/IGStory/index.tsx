d5f3a7ae1f3b223e884e8741fe756d5023438541
jacobhsu <jacob.hsu.tw@gmail.com>
2022-05-09 12:07:46 +0800

    feat: process.env.NODE_ENV

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -1,11 +1,12 @@
 import Item from "./Item";
 import { useGetIGStoiesQuery } from "services/homeServices";
 import Loading from "components/Loading";
-// import db from  "../../../../db.json";
+import db from  "../../../../db.json";
 
 const IGStory: React.FC = () => {
-  // const { stories } = db;
+  const { stories } = db;
   const { data, isLoading } = useGetIGStoiesQuery("all");
+  const story = process.env.NODE_ENV === 'production' ? stories : data;
 
   return (
     <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8">
@@ -14,7 +15,7 @@ const IGStory: React.FC = () => {
           <Loading />
         </div>
       )}
-      { !isLoading && data?.map((item) => {
+      { !isLoading && story?.map((item) => {
           const { id, name, avatar } = item;
           return <Item key={id} name={name} avatar={avatar} />;
         })}
